4ce5c20ca8d4a98bba6d6d2a7f3184cb40cf3990
Valerie Russell <vruss14@gmail.com>
2021-04-28 16:54:41 -0400

    added local storage so that posts can be edited later on

--- public/js/write.js
+++ public/js/write.js
@@ -11,38 +11,71 @@ let newArr = [];
 titleBtn.addEventListener('click', makeTitle)
 
 function makeTitle() {
-    // Formats the user's title and clears any previous update
-    let userTitle = titleInput.value.toUpperCase();
-    titleContainer.innerHTML = '';
 
-    let createdTitle = document.createElement("h1");
-    createdTitle.classList.add('ml-5', 'mt-5', 'display-4', 'custom-title');
-    createdTitle.setAttribute('id', 'post-title');
-    createdTitle.textContent = userTitle;
-    titleContainer.appendChild(createdTitle);
+    let userTitle = titleInput.value;
+    localStorage.setItem("savedtitle", userTitle);
+
+    // Formats the user's title and clears any previous update; the server will need these instructions later on
+    
+    // let userTitle = titleInput.value.toUpperCase();
+    // titleContainer.innerHTML = '';
+    
+    // let createdTitle = document.createElement("h1");
+    // createdTitle.classList.add('ml-5', 'mt-5', 'display-4', 'custom-title');
+    // createdTitle.setAttribute('id', 'post-title');
+    // createdTitle.textContent = userTitle;
+    // titleContainer.appendChild(createdTitle);
 }
+
+function retrieveTitle() {
+    let savedTitle = localStorage.getItem("savedtitle");
+
+    if (savedTitle === null || savedTitle === undefined) {
+        titleInput.value = "";
+    } else {
+        titleInput.value = savedTitle;
+    }
+}
+
+retrieveTitle();
 
 contentBtn.addEventListener('click', makeContent);
 
 function makeContent() {
 
-    // Grabs the user's blog post text and splits it based on line breaks (paragraphs)
     let newContent = userContent.value;
-    newArr = newContent.split('\n')
+    localStorage.setItem("savedcontent", newContent);
 
-    let filteredArr = newArr.filter(function (e) {
-        return e != "";
-    });
+    // Grabs the user's blog post text and splits it based on line breaks (paragraphs)
+    // let newContent = userContent.value;
+    // newArr = newContent.split('\n')
+
+    // let filteredArr = newArr.filter(function (e) {
+    //     return e != "";
+    // });
 
     // Clears any previously written text
-    contentContainer.innerHTML = '';
+    // contentContainer.innerHTML = '';
 
     // Creates a paragraph for each of the paragraphs the user wrote in the textarea box
-    for (let i = 0; i < filteredArr.length; i++) {
-        let paragraphs = document.createElement('p');
-        paragraphs.classList.add('ml-5', 'mr-5', 'custom-post');
-        paragraphs.textContent = filteredArr[i];
-        contentContainer.appendChild(paragraphs);
-    }
+    // for (let i = 0; i < filteredArr.length; i++) {
+    //     let paragraphs = document.createElement('p');
+    //     paragraphs.classList.add('ml-5', 'mr-5', 'custom-post');
+    //     paragraphs.textContent = filteredArr[i];
+    //     contentContainer.appendChild(paragraphs);
+    // }
 
 }
+
+function retrieveContent() {
+    let savedContent = localStorage.getItem("savedcontent");
+    console.log(savedContent);
+
+    if (savedContent === null || savedContent === undefined) {
+        userContent.value = "";
+    } else {
+        userContent.value = savedContent;
+    }
+}
+
+retrieveContent();
