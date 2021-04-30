12746ed349976b4dd67f393ddf5109c559d6ea8b
Valerie Russell <vruss14@gmail.com>
2021-04-30 17:22:08 -0400

    began building log in functionality for the site

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,53 @@
-
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+  
+    if (username && password) {
+        console.log("Username and password are valid!");
+        req.session.logged_in = true;
+        window.location = 'dashboard';
+        
+    // Send a POST request to the API endpoint
+      const response = await fetch('/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    } else {
+        console.log("Failed");
+    }
+  };
+  
+//   const signupFormHandler = async (event) => {
+//     event.preventDefault();
+  
+//     const name = document.querySelector('#name-signup').value.trim();
+//     const email = document.querySelector('#email-signup').value.trim();
+//     const password = document.querySelector('#password-signup').value.trim();
+  
+//     if (name && email && password) {
+//       const response = await fetch('/api/users', {
+//         method: 'POST',
+//         body: JSON.stringify({ name, email, password }),
+//         headers: { 'Content-Type': 'application/json' },
+//       });
+  
+//       if (response.ok) {
+//         document.location.replace('/profile');
+//       } else {
+//         alert(response.statusText);
+//       }
+//     }
+//   };
+  
+  document.getElementById('login-btn').addEventListener('click', loginFormHandler);
+//   document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
