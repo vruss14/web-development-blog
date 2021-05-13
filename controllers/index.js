24d8e5c59ffdf2858810f0cb220b8115feb2837d
Valerie Russell <vruss14@gmail.com>
2021-05-13 15:03:10 -0400

    fixed case sensitive issue for Heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const homeRoutes = require('../controllers/homeRoutes');
+const homeRoutes = require('./homeroutes');
 const apiRoutes = require('../controllers/api');
 
 router.use('/', homeRoutes);
